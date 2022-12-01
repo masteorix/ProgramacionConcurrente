d2bad3ec57bf1f9625aa988870de4d155422a1f3
Andres <52229171+masteorix@users.noreply.github.com>
2022-12-01 17:42:37 +0100

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -1,163 +1,39 @@
-#!/usr/bin/env python
-# coding: utf-8
 import threading
 import sys
 import socket
 import pickle
 import os
 
-
 class Cliente():
 
-    def __init__(self, host=input("Intoduzca la IP del servidor ?  "), port=int(input("Intoduzca el PUERTO del servidor ?  ")), nickname=input("Cual es tu nickname ? ")):
-        self.s = socket.socket()
-        self.s.connect((host, int(port)))
-        print('\n\tProceso con PID = ', os.getpid(), '\n\tHilo PRINCIPAL con ID =', threading.currentThread().getName(), '\n\tHilo en modo DAEMON = ',
-              threading.currentThread().isDaemon(), '\n\tTotal Hilos activos en este punto del programa =', threading.active_count())
-        threading.Thread(target=self.recibir, daemon=True).start()
+	def __init__(self, host=socket.gethostname(), port=59989):
+		self.sock = socket.socket()
+		self.sock.connect((str(host), int(port)))
+		hilo_recv_mensaje = threading.Thread(target=self.recibir)
+		hilo_recv_mensaje.daemon = True
+		hilo_recv_mensaje.start()
+		print('Hilo con PID',os.getpid())
+		print('Hilos activos', threading.active_count())
 
-        while True:
-            msg = input(
-                '\nEscriba texto ?   ** Enviar = ENTER   ** Salir Chat = 1 \n')
-            with open("u22251035AI1.txt", "a") as f:
-                f.write("\n" + nickname + ":" + msg)
-                f.close()
-            if msg != '1':
-                #msg = nickname + ":  " + msg
-                self.enviar(msg)
-            else:
-                print(
-                    " **** Me piro vampiro; cierro socket y mato al CLIENTE con PID = ", os.getpid())
-                self.s.close()
-                sys.exit()
+		while True:
+			msg = input('\nEscriba texto ? ** Enviar = ENTER ** Abandonar Chat = Q \n')
+			if msg != 'Q' :
+				self.enviar(msg)
+			else:
+				print(" **** TALOGOOO  ****")
+				self.sock.close()
+				sys.exit()
 
-    def sec_mult(self, A, B):
-        print("Secuencial:")
-        n_fil_A = len(A)  # Obtengo num de filas de A
-        print(n_fil_A)
-        n_col_A = len(A[0])  # Obtengo num de colunmas de A
-        print(n_col_A)
-        n_fil_B = len(B)  # Obtengo num de filas de B
-        print(n_fil_B)
-        n_col_B = len(B[0])
-        print(n_col_B)
-        if n_col_A != n_fil_B:
-            print("Dimension invalida")
-        else : 
-            # Crear y poblar la matrix  C = A*B
-            C = [[0] * n_col_B for i in range(n_fil_A)]
-            # Hago la multiplicacion de AxB = C, i para iterar sobre las filas de A
-            for i in range(n_fil_A):
-                for j in range(n_col_B):  # j para iterar sobre las columnas de B
-                    for k in range(n_col_A):  # k para iterar en C
-                        # Aqui se hace la multiplicación y guardo en C.
-                        C[i][j] += A[i][k] * B[k][j]
-            return C
-    
-    def par_mult(self,A, B):  # f() que prepara el reparto de trabajo para la mult. en paralelo
-        import multiprocessing as mp
-        import math
-        print("Paralelo:")
-        # Columnas  a procesar x c/cpre, ver Excel adjunto
-        n_fil_A = len(A)  # Obtengo num de filas de A
-        print(n_fil_A)
-        n_col_A = len(A[0])  # Obtengo num de colunmas de A
-        print(n_col_A)
-        n_fil_B = len(B)  # Obtengo num de filas de B
-        print(n_fil_B)
-        n_col_B = len(B[0])
-        print(n_col_B)
-        n_cores = mp.cpu_count()  # Obtengo los cores de mi pc
-        size_col = math.ceil(n_col_B/n_cores)
-        # Filas a procesar x c/cpre, ver Excel adjunto
-        size_fil = math.ceil(n_fil_A/n_cores)
-        # Array MC de memoria compartida donde se almacenaran los resultados, ver excel adjunto
-        MC = mp.RawArray('i', n_fil_A * n_col_B)
-        cores = []  # Array para guardar los cores y su trabajo
-        # Asigno a cada core el trabajo que le toca, ver excel adjunto
-        print("heeeey")
-        for core in range(n_cores):
-            # Calculo i para marcar inicio del trabajo del core en relacion a las filas
-            i_MC = min(core * size_fil, n_fil_A)
-            # Calculo f para marcar fin del trabajo del core, ver excel
-            f_MC = min((core + 1) * size_fil, n_fil_A)
-            # Añado al Array los cores y su trabajo
-            cores.append(mp.Process(target=self.par_core, args=(A, B, MC, i_MC, f_MC)))
-        for core in cores:
-            core.start()  # Arranco y ejecuto el trabajo para c/ uno de los cores que tenga mi equipo, ver excel
-        for core in cores:
-            core.join()  # Bloqueo cualquier llamada hasta que terminen su trabajo todos los cores
-        # Convierto el array unidimensional MC en una matrix 2D (C_2D)
-        C_2D = [[0] * n_col_B for i in range(n_fil_A)]
-        for i in range(n_fil_A):  # i para iterar sobre las filas de A
-            for j in range(n_col_B):  # j para iterar sobre las columnas de B
-                # Guardo el C_2D los datos del array MC
-                C_2D[i][j] = MC[i*n_col_B + j]
-        return C_2D
+	def recibir(self):
+		while True:
+			try:
+				data = self.sock.recv(32)
+				if data:
+					print(pickle.loads(data))
+			except:
+				pass
 
+	def enviar(self, msg):
+		self.sock.send(pickle.dumps(msg))
 
-    def par_core(A, B, MC, i_MC, f_MC):  # La tarea que hacen todos los cores
-        # Size representado en colores en el excel que itera sobre las filas en A
-        for i in range(i_MC, f_MC):
-            # Size representado en colores en el excel que itera sobre las columnas en B
-            for j in range(len(B[0])):
-                for k in range(len(A[0])):  # n_fil_B o lo que es l mismo el n_col_A
-                    # Guarda resultado en MC[] de cada core
-                    MC[i*len(B[0]) + j] += A[i][k] * B[k][j]
-
-    def recibir(self):
-
-        print('\nHilo RECIBIR con ID =', threading.currentThread().getName(
-        ), '\n\tPertenece al PROCESO con PID', os.getpid(), "\n\tHilos activos TOTALES ", threading.active_count())
-        while True:
-            try:
-                data = self.s.recv(64)
-                data1 = pickle.loads(data)
-                print(type(data1))
-                # data2=np.array(data1)
-                # print(data1[1])
-                # print("1")
-                import random
-
-                # print("2")
-                # Genero A[21535220][6]con num. aleatorios del 0 al 215, ver excel
-                A = [[random.randint(0, 10) for i in range(data1[1])]for j in range(data1[0])]
-                # Genero B[6][21535220]con num. aleatorios del 0 al 215, ver excel
-                B = [[random.randint(0, 10) for i in range(data1[2])]for j in range(data1[1])]
-                # print("3")
-                print(A)
-                print(B)
-                #matriz_result = self.sec_mult(A, B)
-
-                matriz_result = self.sec_mult(A, B)
-                print(matriz_result)
-                #matriz_result2 = self.par_mult(A, B)
-                #print(matriz_result2)
-                '''
-                if data:
-                    
-                    data=pickle.loads(data)
-                    print(type(data))
-                    data1=int(data)+4
-                    print(data1
-                    print(data1)
-                    print(type(data1))
-                    data2=int(data1)
-                    print(type(data2
-                    try:
-                        data1=int(data1)+12
-                        print("hola"+type(data1))
-                        print(data1)
-                    except:
-                        print(type(data1))
-                        print(data1)
-                '''
-
-            except:
-                pass
-
-    def enviar(self, msg):
-        self.s.send(pickle.dumps(msg))
-
-
-arrancar = Cliente()
+c = Cliente()
--- servidor.py
+++ servidor.py
@@ -3,79 +3,61 @@ import threading
 import sys
 import pickle
 import os
-import re as reg
 
-
-class Servidor():  # se hace una clase para llamarla como el main en c
-
-	# saber el puerto e ip del host
-	def __init__(self, host=socket.gethostname(), port=int(input("Que puerto quiere usar ? "))):
+class Servidor():
+	def __init__(self, host=socket.gethostname(), port=59989):
 		self.clientes = []
-		self.nicknames = []
-		print('\nSu IP actual es : ', socket.gethostbyname(host))
-		print('\n\tProceso con PID = ', os.getpid(), '\n\tHilo PRINCIPAL con ID =', threading.currentThread().getName(), '\n\tHilo en modo DAEMON = ',
-		      threading.currentThread().isDaemon(), '\n\tTotal Hilos activos en este punto del programa =', threading.active_count())
-		self.s = socket.socket()
-		self.s.bind((str(host), int(port)))  # enlazar el host y el puerto
-		self.s.listen(30)  # espera a que conexiones entren
-		self.s.setblocking(False)
+		self.sock = socket.socket()
+		self.sock.bind((str(host), int(port)))
+		self.sock.listen(20)
+		self.sock.setblocking(False)
 
-		# evitar race condition
-		threading.Thread(target=self.aceptarC, daemon=True).start()
-		threading.Thread(target=self.procesarC, daemon=True).start()
+		aceptar = threading.Thread(target=self.aceptarC)
+		procesar = threading.Thread(target=self.procesarC)
+		
+		aceptar.daemon = True
+		aceptar.start()
+
+		procesar.daemon = True
+		procesar.start()
 
 		while True:
-			stri=[22,2,4]
-			# FxC x CxP
-			msg = input('\n << SALIR = 1 >> \n')
-			if msg == '1':
-				print(" **** Me piro vampiro; cierro socket y mato SERVER con PID = ", os.getpid())
-				self.s.close()
+			msg = input('SALIR = Q\n')
+			if msg == 'Q':
+				print("**** TALOGOOO *****")
+				self.sock.close()
 				sys.exit()
-#			else: pass
 			else:
-				for c in self.clientes:
-					c.send(pickle.dumps(stri))
-              
+				pass
+
+	def broadcast(self, msg, cliente):
+		for c in self.clientes:
+			try:
+				if c != cliente:
+					c.send(msg)
+			except:
+				self.clientes.remove(c)
+
 	def aceptarC(self):
-		print('\nHilo ACEPTAR con ID =',threading.currentThread().getName(), '\n\tHilo en modo DAEMON = ', threading.currentThread().isDaemon(),'\n\tPertenece al PROCESO con PID', os.getpid(), "\n\tHilos activos TOTALES ", threading.active_count())
 		while True:
 			try:
-				conn, addr = self.s.accept()
-				print(f"\nConexion aceptada via {addr}\n")
+				conn, addr = self.sock.accept()
+				print(f"\nConexion aceptada via {conn}\n")
 				conn.setblocking(False)
 				self.clientes.append(conn)
-			except: pass
+			except:
+				pass
 
 	def procesarC(self):
-		print('\nHilo PROCESAR con ID =',threading.currentThread().getName(), '\n\tHilo en modo DAEMON = ', threading.currentThread().isDaemon(),'\n\tPertenece al PROCESO con PID', os.getpid(), "\n\tHilos activos TOTALES ", threading.active_count())  
+		print("Procesamiento de mensajes iniciado")
 		while True:
-			if len(self.clientes) > 0:   ##si no hay clientes conectados no se procesa
+			if len(self.clientes) > 0:
 				for c in self.clientes:
 					try:
-						data = c.recv(64)                   ##espero y si hay alguien hago el broadcast
-						file=open("data.txt",'wb')
-						file.write(pickle.loads(data))
-						if data: self.broadcast(data,c)
-					except: pass
+						data = c.recv(32)
+						if data:
+							self.broadcast(data,c)
+					except:
+						pass
 
-	def broadcast(self, msg, cliente):        ##enviar el mensaje a todos
-		str=pickle.loads(msg)
-		found=reg.search('^[^ :]*',str)
-		if found.group() not in self.nicknames:
-			self.nicknames.append(found.group())
-		with open("u22045519AI1.txt", "a") as f:
-			f.write("\n" + str)
-			f.close()
-		for c in self.clientes:               ## decir cuantos clientes hay conectados
-			print("Clientes conectados Right now = ", len(self.clientes))
-			print("LISTA DE CLIENTES (NICKNAMES)")
-			for n in self.nicknames:
-				print(n)
-			try:
-				if c != cliente:              ## si cliente != array cliente cargo el mensaje y lo envio a todos
-					print(pickle.loads(msg))
-					c.send(msg)
-			except: self.clientes.remove(c)   ## si falla entonces no hay clientes y lo quito
-
-arrancar = Servidor() 
+s = Servidor()
